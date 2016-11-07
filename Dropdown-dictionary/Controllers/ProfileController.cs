4be431f5891dccee921de97cb360bab0c118092b
kbf <kferrio@gmail.com>
2016-11-06 19:09:03 -0700

    add Role and Facilities

--- Dropdown-dictionary/Controllers/ProfileController.cs
+++ Dropdown-dictionary/Controllers/ProfileController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Dropdowns.Models;
+using System.Linq;
 
 namespace Dropdowns.Controllers
 {
@@ -16,6 +17,8 @@ namespace Dropdowns.Controllers
 
             // Simulate getting states from a database
             model.States = GetStatesFromDB();
+            model.Roles = GetRolesFromDB();
+            model.FacilityList = GetFacilityListFromDB();
 
             return View(model);
         }
@@ -29,6 +32,8 @@ namespace Dropdowns.Controllers
             // Set States on the model. We need to do this because only the value selected 
             // in the DropDownList is posted back, not the whole list of states
             model.States = GetStatesFromDB();
+            model.Roles = GetRolesFromDB();
+            model.FacilityList = GetFacilityListFromDB();
 
             // In case everything is fine - i.e. "FirstName", "LastName" and "State" are entered/selected,
             // redirect a user to the "ViewProfile" page, and pass the user object along via Session
@@ -56,6 +61,12 @@ namespace Dropdowns.Controllers
             // Get a human-readable description of a currently selected State
             var allStates = GetStatesFromDB();
             model.StateName = allStates[model.State];
+
+            var allRoles = GetRolesFromDB();
+            model.RoleName = allRoles[model.Role];
+
+            var allFacilities = GetFacilityListFromDB();
+            model.FacilitiesNames = (model.Facilities).Where(allFacilities.ContainsKey).Select(i => allFacilities[i]).ToArray();
 
             // Display View Profile page that shows FirstName, Last Name and a selected State.
             return View(model);
@@ -120,6 +131,34 @@ namespace Dropdowns.Controllers
                 {"WI", "Wisconsin"},
                 {"WV", "West Virginia"},
                 {"WY", "Wyoming"}
+            };
+        }
+
+        /// <summary>
+        /// Simulates retrieval of country's states from a DB.
+        /// </summary>
+        /// <returns>Dictionary of US states</returns>
+        private Dictionary<string, string> GetRolesFromDB()
+        {
+            return new Dictionary<string, string>
+            {
+                {"6771570A-75E0-4217-9222-70EEC716F46A", "User"},
+                {"C7F6D623-8F51-45F6-8F43-E26F003C1B7E", "Data Manager"},
+                {"F9994BA5-96F8-464A-B0A8-2E093685EE71", "Administrator"},
+            };
+        }
+
+        /// <summary>
+        /// Simulates retrieval of country's states from a DB.
+        /// </summary>
+        /// <returns>Dictionary of US states</returns>
+        private Dictionary<string, string> GetFacilityListFromDB()
+        {
+            return new Dictionary<string, string>
+            {
+                {"820C87A6-9D97-4DD1-B2F6-B95343AC7501", "Facility 1"},
+                {"820C87A6-9D97-4DD1-B2F6-B95343AC7502", "Facility 2"},
+                {"820C87A6-9D97-4DD1-B2F6-B95343AC7503", "Facility 3"},
             };
         }
     }
--- Dropdown-dictionary/Models/UserProfileModel.cs
+++ Dropdown-dictionary/Models/UserProfileModel.cs
@@ -19,10 +19,23 @@ namespace Dropdowns.Models
         [Display(Name = "State")]
         public string State { get; set; }
 
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+
+        [Required]
+        [Display(Name = "Facilities")]
+        public IEnumerable<string> Facilities { get; set; }
+
+
         // This property holds all available states for selection
         public Dictionary<string, string> States { get; set; }
+        public Dictionary<string, string> Roles { get; set; }
+        public Dictionary<string, string> FacilityList { get; set; }
 
         // Property to store human-readable state name
         public string StateName { get; set; }
+        public string RoleName { get; set; }
+        public string[] FacilitiesNames { get; set; }
     }
 }
